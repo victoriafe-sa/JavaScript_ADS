2c65216642bca1a02ce609fdf2fdf1bfbf030b7c
Victoria Ferreira <victoria.senac.13@gmail.com>
2025-03-11 11:30:04 -0300

    .

---
+++ atividade.js
@@ -0,0 +1,36 @@
+// Tipos de Dados
+
+//String
+let nome = 'Victoria'
+
+// Numero
+let n1 = 10
+
+//BigInt
+let big = 12345678
+
+//Boolean
+let chuva = true
+
+//Undefined
+let naoDef
+
+//Null
+let vazio = null
+
+//Symbol
+let simb = Symbol('descricao')
+
+//Object
+let pessoa ={
+    idade: 10,
+    nome: 'Maria'
+}
+
+//Array ou Vetor 
+let frutas = ['maçã','banana','uva']
+
+// let é usado para declarar uma variavel, com possibilidade de atualizar ela, porém não pode ser declarada novamente.
+//já o const não pode ser atualizado.  
+
+//A função typeof do JavaScript serve para determinar o tipo de uma variável ou expressão. É uma palavra-chave que retorna uma string indicando o tipo do dado
